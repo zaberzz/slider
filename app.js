7e5e557fe4953ca5325b8f5063e44c8851cdc5cc
Saba <saba.sadegh67@gmail.com>
2022-12-13 10:37:45 +0330

    working with slides

--- app.js
+++ app.js
@@ -17,6 +17,12 @@ prevBtn.addEventListener("click", function () {
 });
 
 function carousel() {
+  if (counter === slides.length) {
+    counter = 0;
+  }
+  if (counter < 0) {
+    counter = slides.length - 1;
+  }
   slides.forEach(function (slide) {
     slide.style.transform = `translateX(-${counter * 100}%)`;
   });
